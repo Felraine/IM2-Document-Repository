07abff162b34a427945a3c8f3b514742bb1174c5
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2024-11-06 01:40:05 +0800

    Edit event button works frontend & backend

--- Django/dashboard_app/urls.py
+++ Django/dashboard_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_view, addEvent,deleteEvent
+from .views import dashboard_view, addEvent,deleteEvent,updateEvent
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),  
     path('addEvent/', addEvent, name='addEvents'),
     path('delete/<int:event_id>/', deleteEvent, name='deleteEvent'),
+    path('updateEvent', updateEvent, name='updateEvent'), 
 ]
